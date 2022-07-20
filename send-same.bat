a487aa73ea524d8771ba079f21924b405abee13f
Sathish Kumar G <36076378+kgsatish@users.noreply.github.com>
2022-07-20 10:14:08 +0530

    Delete send-same.bat

--- send-same.bat
+++
@@ -1,2 +0,0 @@
-python app.py --img "C:\software\python\whatsapp-direct-bulk\rajani-1.jpg" rajani.xlsx rajani-1.txt
-python app.py --img "C:\software\python\whatsapp-direct-bulk\rajani-2.jpg" rajani.xlsx rajani-2.txt
